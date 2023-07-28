709f579aaf69fed8d7a40713661f8a922421d87a
necro-01 <prit.suvagiya01@gmail.com>
2023-07-28 16:50:49 +0530

    role based auth

--- src/main/java/com/blog/controllers/AuthController.java
+++ src/main/java/com/blog/controllers/AuthController.java
@@ -1,9 +1,11 @@
 package com.blog.controllers;
 
+import com.blog.entities.User;
 import com.blog.exceptions.InvalidCredentialException;
 import com.blog.payloads.JwtAuthRequest;
 import com.blog.payloads.JwtAuthResponse;
 import com.blog.payloads.UserDto;
+import com.blog.repositories.UserRepo;
 import com.blog.security.JwtTokenHelper;
 import com.blog.services.UserService;
 import org.modelmapper.ModelMapper;
@@ -14,10 +16,13 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/api/v1/auth/")
@@ -28,13 +33,15 @@ public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final UserService userService;
     private final ModelMapper modelMapper;
+    private final UserRepo userRepo;
 
-    public AuthController(JwtTokenHelper jwtTokenHelper, UserDetailsService userDetailsService, AuthenticationManager authenticationManager, UserService userService, ModelMapper modelMapper) {
+    public AuthController(JwtTokenHelper jwtTokenHelper, UserDetailsService userDetailsService, AuthenticationManager authenticationManager, UserService userService, ModelMapper modelMapper, UserRepo userRepo) {
         this.jwtTokenHelper = jwtTokenHelper;
         this.userDetailsService = userDetailsService;
         this.authenticationManager = authenticationManager;
         this.userService = userService;
         this.modelMapper = modelMapper;
+        this.userRepo = userRepo;
     }
 
     @PostMapping("/login")
@@ -62,5 +69,11 @@ public class AuthController {
             System.out.println("Invalid credentials");
             throw new InvalidCredentialException("Invalid credentials");
         }
+    }
+
+    @GetMapping("/current-user/")
+    public ResponseEntity<UserDto> getUser(Principal principal) {
+        User user = this.userRepo.findByEmail(principal.getName()).get();
+        return new ResponseEntity<UserDto>(this.modelMapper.map(user, UserDto.class), HttpStatus.OK);
     }
 }
